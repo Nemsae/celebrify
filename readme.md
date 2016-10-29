c4711144565afd061a41b4f3c33d669d15f560ab
John Son <jsson77@gmail.com>
2016-10-29 00:01:17 -0700

    v1.0.0

---
+++ readme.md
@@ -0,0 +1,19 @@
+## sleazycase
+
+[![CircleCI](https://circleci.com/gh/Nemsae/sleazycase.svg?style=shield)](https://circleci.com/gh/Nemsae/sleazycase)
+
+Alternate string casing
+
+## Install
+
+```bash
+$ npm install sleazycase
+```
+
+## Usage
+```js
+const sleazycase = require('sleazycase');
+
+sleazycase('lets get parsed') // --> 'lEtS GeT PaRsEd'
+sleazycase('SOME STRING') // --> 'sOmE StRiNg'
+```
---
+++ test.js
@@ -0,0 +1,36 @@
+// const assert = require('chai').assert;
+const { expect, assert } = require('chai');
+
+const celebrify = require('./') //  automatically resolve to index.js
+
+describe('celebrify', () => {
+  it ('returns a random quote', () => {
+    // assert.equal(celebrify('abcdefg'), 'aBcDeFg');
+    expect(celebrify('random')).to.be.a('string');
+    expect(celebrify('jayden')).to.be.a('string');
+    expect(celebrify('Bush')).to.be.a('string');
+    expect(celebrify('DJ Khaled')).to.be.a('string');
+    // assert.equal(celebrify('ABCDEFG'), 'aBcDeFg');
+    // assert.equal(celebrify('a cde g'), 'a cDe g');
+    // assert.equal(celebrify('  abc  '), '  aBc  ');
+  });
+
+  it('handles no category', () => {
+    // expect(celebrify('Obama')).to.throw('Listed category, input must be.');
+    // assert.throws(() => celebrify('Obama'));
+  });
+
+  it ('throws with non-string input', () => {
+    assert.throws(() => celebrify({}));
+    assert.throws(() => celebrify([]));
+    assert.throws(() => celebrify(undefined));
+    assert.throws(() => celebrify(123));
+    assert.throws(() => celebrify(0));
+    assert.throws(() => celebrify(true));
+    assert.throws(() => celebrify(false));
+  });
+
+  it ('throws with missing input', () => {
+    assert.throws(() => celebrify());
+  });
+});
